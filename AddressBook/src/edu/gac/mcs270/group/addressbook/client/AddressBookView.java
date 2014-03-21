c677fbb3c67be8c2b9834a9b1f8ae33d6da61d30
Ian Elletson <ianelletson@gmail.com>
2014-03-21 14:16:53 -0500

    breaking stuff. sweet

---
+++ AddressBook/src/edu/gac/mcs270/group/addressbook/client/AddressBookView.java
@@ -0,0 +1,149 @@
+package edu.gac.mcs270.group.addressbook.client;
+
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
+import com.google.gwt.user.client.ui.MenuItemSeparator;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Class that is shown to user when visiting page, what the user interacts with
+ * @author ielletso
+ *
+ */
+public class AddressBookView {
+	private AddressBook control;
+	
+	/**
+	 * Default constructor
+	 */
+	public AddressBookView() {}
+	
+	public void setController(AddressBook adBook) {
+		control = adBook;
+	}
+	
+	/**
+	 * Sets up welcome page i.e. first loaded page on visit
+	 */
+	public void viewWelcomePage() {
+		RootPanel rootPanel = RootPanel.get();
+		rootPanel.clear();
+		makeMenuBar(rootPanel);
+		
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		rootPanel.add(horizontalPanel, 10, 79);
+//		We don't need a side bar on the welcome page
+//		makeViewSideBar(horizontalPanel);
+	}
+	
+	public void viewBookPage() {
+		RootPanel rp = RootPanel.get();
+		rp.clear();
+		makeMenuBar(rp);
+		
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		rp.add(horizontalPanel, 10, 79);
+		
+		makeViewSideBar(horizontalPanel);
+	}
+
+	private void makeViewSideBar(HorizontalPanel hp) {
+		// TODO Make click handlers
+		VerticalPanel sidePanel = new VerticalPanel();
+		hp.add(sidePanel);
+		sidePanel.setSize("72px", "98px");
+
+		Button print = new Button("Print");
+		print.setStyleName("sideBarButton");
+		print.setText("Print");
+//		TODO: Print click handler
+//		print.addClickHandler(new ClickHandler() {
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				viewPostAdForm();
+//			}
+//	      });
+		sidePanel.add(print);
+
+		Button sortZipButton = new Button("Sort by Zip");
+		sortZipButton.setStyleName("sideBarButton");
+		sortZipButton.setText("Sort by Zip");
+		sidePanel.add(sortZipButton);
+		
+//		TODO: Add click handler for sort by zip
+//		sortZipButton.addClickHandler(new ClickHandler() {
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				control.viewAdDataFromServer();
+//			}
+//	      });
+//		sidePanel.add(sortZipButton);
+		
+		Button sortNameButton = new Button("Sort by Name");
+		sortNameButton.setStyleName("sideBarButton");
+		sortNameButton.setText("Sort by Name");
+//		TODO: add click handler for sort by name
+//		sortNameButton.addClickHandler(new ClickHandler() {
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				control.viewAdDataFromServer();
+//			}
+//	      });
+		sidePanel.add(sortNameButton);
+	}
+
+	/**
+	 * Make a menu bar on the top of the page that displays commonly used items
+	 * @param rp the root panel of the page
+	 */
+	private void makeMenuBar(RootPanel rp) {
+		// TODO Implement methods and button listeners/click handlers
+		//  is root panel
+		MenuBar menuBar = new MenuBar(false);
+		rp.add(menuBar, 94, 39);
+		menuBar.setSize("326px", "32px");
+
+		MenuItem menuHomeItem = new MenuItem("Home", false, new Command() {
+			public void execute() {
+				viewWelcomePage();
+			}
+		});
+		menuHomeItem.setHTML("Home");
+		menuBar.addItem(menuHomeItem);
+		
+		menuBar.addSeparator(new MenuItemSeparator());
+
+		// TODO this looks really bad if we have "Address Book" as opposed to book. We need to fix this
+		MenuItem menuViewAdBook = new MenuItem("Address Book", false, new Command() {
+			public void execute() {
+				viewBookPage();
+			}
+		});
+		menuViewAdBook.setHTML("Book");
+		menuBar.addItem(menuViewAdBook);
+		
+		
+		
+		menuBar.addSeparator(new MenuItemSeparator());
+
+		MenuItem menuAddEntry = new MenuItem("Add Entry", false, (Command) null);
+		menuAddEntry.setHTML("Add Entry");
+		menuBar.addItem(menuAddEntry);
+		menuBar.addSeparator(new MenuItemSeparator());
+		
+		MenuItem menuSearchItem = new MenuItem("Search", false, (Command) null);
+		menuSearchItem.setHTML("Search");
+		menuBar.addItem(menuSearchItem);
+
+	}
+}
---
+++ AddressBook/src/edu/gac/mcs270/group/addressbook/shared/AddressEntry.java
@@ -0,0 +1,96 @@
+package edu.gac.mcs270.group.addressbook.shared;
+
+import java.io.Serializable;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+/**
+ * 
+ * @author ielletso
+ * class with fields for name, address, city, state, zip, phone
+ */
+@PersistenceCapable(identityType=IdentityType.APPLICATION)
+public class AddressEntry implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
+	private Long id;
+	
+	/**
+	 * Our private fields that are persistent and save-able
+	 */
+	@Persistent
+	private String name ="No name";
+	@Persistent
+	private String address = "No address";
+	@Persistent
+	private String city = "Nowhere";
+	@Persistent
+	private String State = "Missouri";
+	@Persistent
+	private int zip = 0;
+	@Persistent
+	private String phoneNumber = "No phone";
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	// TODO make this print out in label form.
+	public String toString() {
+		return "Address entry: [name=" + name + ", address=" + address
+				+ ", city=" + city + ", State=" + State + ", zip=" + zip
+				+ ", phoneNumber=" + phoneNumber + "]";
+	}
+	
+	
+}
